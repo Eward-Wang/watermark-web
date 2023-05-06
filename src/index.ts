efe3a715fc3648cebd4d3d2845560c9354a78fd3
eward <ewardwang@126.com>
2023-05-06 12:22:35 +0800

    feat: use canvas impl

--- src/index.ts
+++ src/index.ts
@@ -62,16 +62,69 @@ function debounce(fn: Function, time: number) {
   };
 }
 
+function generateImage(option: {
+  fontSize?: number;
+  text: string;
+  paddingX?: number;
+  paddingY?: number;
+  angle?: number;
+  alpha?: number;
+}) {
+  if (!option.text) throw new Error("text is invalid!");
+  const fontSize = option.fontSize || 16;
+  const text = option.text || "";
+  const paddingX = option.paddingX || 32;
+  const paddingY = option.paddingY || 16;
+  const font = `${fontSize}px sans-serif `;
+  const color = `rgba(0,0,0,${option.alpha || 0.3})`;
+  const angle = option.angle || 15;
+
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+  ctx.font = font;
+  ctx.fillStyle = color;
+  const { width: fontWidth } = ctx.measureText(text);
+  const fontHeight = fontSize;
+  const actualDiagonal =
+    fontHeight / 2 / Math.tan((90 - angle) * (Math.PI / 180)) +
+    fontHeight / 2 / Math.tan((angle * Math.PI) / 180) +
+    fontWidth;
+  const actualHeight =
+    (Math.abs(Math.sin(angle * (Math.PI / 180))) * actualDiagonal +
+      paddingY * 2) *
+    window.devicePixelRatio;
+  const actualWidth =
+    (Math.abs(Math.cos(angle * (Math.PI / 180))) * actualDiagonal +
+      paddingX * 2) *
+    window.devicePixelRatio;
+
+  canvas.width = actualWidth;
+  canvas.height = actualHeight;
+
+  ctx.translate(actualWidth / 2, actualHeight / 2);
+  ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+
+  ctx.rotate(-angle * (Math.PI / 180));
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText(text, 0, 0);
+  const result = canvas.toDataURL("image/png", 1);
+  return result;
+}
+
 const isObject = (obj: any) =>
   obj && Object.prototype.toString.call(obj) === "[object Object]";
 
 class Watermark {
   private setting: watermarkSettingType = null;
-  private wrapper: HTMLElement = null;
   private readonly update: () => void = null;
-  private uuid = uuid();
   private _refreshing = false;
+  private wrapper: HTMLDivElement;
+
   private observer: MutationObserver;
+  private observerWrapper: MutationObserver;
 
   constructor(setting?: watermarkSettingType) {
     this.setting = {
@@ -90,13 +143,43 @@ class Watermark {
   public init = () => {
     this.build();
 
-    this.observer = new MutationObserver(this.observeCallback);
+    this.observer = new MutationObserver(this.observerCallback);
     this.observer.observe(document.body, {
-      attributes: true,
       childList: true,
-      subtree: true,
     });
-    window.addEventListener("resize", this.update);
+  };
+  private observerCallback = (mutations: MutationRecord[]) => {
+    if (this._refreshing) return;
+    let wasChanged = false;
+    for (const r of mutations) {
+      if (r.target === document.body) {
+        for (const n of r.removedNodes) {
+          if (n === this.wrapper) {
+            wasChanged = true;
+            break;
+          }
+        }
+      }
+    }
+    if (wasChanged) {
+      this._refreshing = true;
+      this.build();
+    }
+  };
+
+  private observerWrapperCallback = (mutations: MutationRecord[]) => {
+    if (this._refreshing) return;
+    let wasChanged = false;
+    for (const r of mutations) {
+      if (r.target === this.wrapper) {
+        wasChanged = true;
+        break;
+      }
+    }
+    if (wasChanged) {
+      this._refreshing = true;
+      this.build();
+    }
   };
 
   // aha destory -> destroy;
@@ -106,12 +189,16 @@ class Watermark {
 
   public destroy() {
     this.observer.disconnect();
-    window.removeEventListener("resize", this.update);
-    const el = document.getElementById(this.uuid);
-    if (el) {
-      document.body.removeChild(el);
+    if (this.wrapper) {
+      if (this.observerWrapper) {
+        this.observerWrapper.disconnect();
+        this.observerWrapper = null;
+      }
+      if (document.body.contains(this.wrapper)) {
+        document.body.removeChild(this.wrapper);
+      }
+      this.wrapper = null;
     }
-    this.wrapper = null;
   }
 
   public change = (param?: Partial<watermarkSettingType>): void => {
@@ -120,95 +207,33 @@ class Watermark {
     this.update();
   };
 
-  private observeCallback = (mutations: MutationRecord[]) => {
-    if (this._refreshing) return;
-    let wasChanged = false;
-    for (const r of mutations) {
-      if (r.target === document.body) {
-        for (const n of r.removedNodes) {
-          if (n === this.wrapper) wasChanged = true;
-        }
-        continue;
-      }
-      if (
-        r.target === this.wrapper ||
-        r.target?.parentNode === this.wrapper ||
-        r.target?.parentNode?.parentNode === this.wrapper
-      ) {
-        wasChanged = true;
-        break;
-      }
-    }
-    if (wasChanged) {
-      this._refreshing = true;
-      this.wrapper = null;
-      this.rebuild();
-    }
-  };
-
-  private gElNode() {
-    const el = document.createElement("span");
-    el.style.cssText = `display: inline-block;padding: ${this.setting.gutterY}px ${this.setting.gutterX}px;opacity: ${this.setting.alpha};`;
-    const span = document.createElement("span");
-    span.style.cssText = `transform: rotate(-${this.setting.angle}deg);display: inline-block;padding: 16px 32px;line-height: 1.5em;`;
-    span.innerText = this.setting.text;
-    el.appendChild(span);
-    return el;
-  }
-
-  private gRowNode() {
-    const el = document.createElement("div");
-    el.style.cssText = `display: flex;justify-content: space-between;`;
-    return el;
-  }
-
-  private gWrapper() {
-    this.wrapper = document.getElementById(this.uuid);
-    if (!this.wrapper) {
-      this.wrapper = document.createElement("div");
-      this.wrapper.setAttribute("id", this.uuid);
-      this.wrapper.style.cssText = `transform: translate3d(0,0,0);pointer-events: none;position: fixed;top: 0;z-index: 9999;width: 100%;height: 100%;overflow: hidden;`;
-      document.body.appendChild(this.wrapper);
-    }
-  }
-
-  private rebuild() {
-    const el = document.getElementById(this.uuid);
-    if (el) document.body.removeChild(el);
-    this.uuid = uuid();
-    this.wrapper = null;
-    this.build();
-  }
-
   private build() {
     this._refreshing = true;
-    if (!this.wrapper) this.gWrapper();
-    this.wrapper.innerHTML = "";
-    const firstChild = this.gElNode();
-    this.wrapper.appendChild(firstChild);
-    const { width, height } = firstChild.getBoundingClientRect();
-    const rowN = Math.ceil(
-      Math.min(document.body.scrollHeight, document.body.clientHeight) /
-        Math.ceil(height)
-    );
-    const colN =
-      (Math.min(document.body.scrollWidth, document.body.clientWidth) /
-        Math.ceil(width)) ^
-      0;
-    this.wrapper.removeChild(firstChild);
-
-    const vel = document.createDocumentFragment();
-    for (let row = 0; row < rowN; row++) {
-      const r = this.gRowNode();
-      for (let col = 0; col < colN; col++) {
-        r.appendChild(this.gElNode());
+    if (this.wrapper) {
+      if (document.body.contains(this.wrapper)) {
+        document.body.removeChild(this.wrapper);
       }
-      vel.appendChild(r);
+      this.observerWrapper.disconnect();
+      this.observerWrapper = null;
+      this.wrapper = null;
     }
-    this.wrapper.appendChild(vel);
-    setTimeout(() => {
-      this._refreshing = false;
-    }, 50);
+    this.wrapper = document.createElement("div");
+    this.wrapper.style.cssText = `
+        overflow: hidden!important;
+        background-image:url(${generateImage(this.setting)}) !important;
+        position: fixed!important;
+        top: 0!important;
+        left: 0!important;
+        width: 100vw!important;
+        height: 100vh!important;
+        z-index: 9999!important;
+        pointer-events: none!important;`;
+    document.body.appendChild(this.wrapper);
+    this.observerWrapper = new MutationObserver(this.observerWrapperCallback);
+    this.observerWrapper.observe(this.wrapper, {
+      attributes: true,
+    });
+    setTimeout(() => (this._refreshing = false));
   }
 }
 
